6e9c0c13882fd6e9df310f960c36b91d8514a5b6
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2024-11-06 20:31:17 +0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
-/* eslint-disable no-unused-vars */
-import Header from "./components/header";
-import Cards from "./components/cards";
+  /* eslint-disable no-unused-vars */
+  import Header from "./components/header";
+  import Cards from "./components/cards";
 
-import ContactUS from "./components/contactUS";
-import Course from "./components/course";
+  import ContactUS from "./components/contactUS";
+  import Course from "./components/course";
 
-function App() {
-  // const [theme, setTheme] = useState('light');
+  function App() {
+    // const [theme, setTheme] = useState('light');
 
-  // useEffect(() => {
-  //   const savedTheme = localStorage.getItem('theme');
-  //   if (savedTheme) {
-  //     setTheme(savedTheme);
-  //     document.documentElement.classList.add(savedTheme);
-  //   }
-  // }, []);
+    // useEffect(() => {
+    //   const savedTheme = localStorage.getItem('theme');
+    //   if (savedTheme) {
+    //     setTheme(savedTheme);
+    //     document.documentElement.classList.add(savedTheme);
+    //   }
+    // }, []);
 
-  return (
-    <>
-      <Header />
-      <Course />
-      <Cards />
-      <ContactUS />
-      <a href="tel:+998909298801">
-        <div className="w-[70px] fixed right-5 bottom-[50px] z-[999] group">
-          <img
-            src="/free_phone.png"
-            alt=""
-            className="w-full animate-bounce group-hover:animate-bounce transition duration-300 ease-in-out"
-          />
-        </div>
-      </a>
-    </>
-  );
-}
+    return (
+      <>
+        <Header />
+        <Course />
+        {/* <Cards /> */}
+        <ContactUS />
+        <a href="tel:+998909298801">
+          <div className="w-[70px] fixed right-5 bottom-[50px] z-[999] group">
+            <img
+              src="/free_phone.png"
+              alt=""
+              className="w-full animate-bounce group-hover:animate-bounce transition duration-300 ease-in-out"
+            />
+          </div>
+        </a>
+      </>
+    );
+  }
 
-export default App;
+  export default App;
--- src/components/course.jsx
+++ src/components/course.jsx
@@ -1,12 +1,41 @@
 /* eslint-disable no-unused-vars */
-import { FaHeart } from "react-icons/fa";
+import { useEffect, useContext } from "react";
 import { getText } from "../language";
 import { LanguageContext } from "../context/LanguageContext";
-import { useContext } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Course = () => {
-  const { selectedLanguage, selectedFlag, changeLanguage } =
-    useContext(LanguageContext);
+  const { selectedLanguage, selectedFlag, changeLanguage } = useContext(LanguageContext);
+
+  useEffect(() => {
+    const courseCards = document.querySelectorAll(".course-card");
+
+    // Kartalarga animatsiyani qo'shish
+    gsap.fromTo(
+      courseCards,
+      { opacity: 0, y: 50, scale: 0.9 },
+      {
+        opacity: 1,
+        y: 0,
+        scale: 1,
+        duration: 1,
+        ease: "power4.out",
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: courseCards,
+          start: "top 80%", // Sahifaning 80% qismini ko'rganida
+          end: "bottom 20%",
+          toggleActions: "play none none none", // Faollashish va qaytish animatsiyasiz
+          onEnter: () => gsap.fromTo(courseCards, { opacity: 0, y: 50, scale: 0.9 }, { opacity: 1, y: 0, scale: 1, duration: 1, ease: "power4.out", stagger: 0.2 }),
+          // Pastga tushganda animatsiya ishlaydi, lekin yuqoriga qaytishda ishlamaydi
+        },
+      }
+    );
+  }, []);
+
   return (
     <>
       <div className="flex justify-center items-center py-20 bg-gray-100">
@@ -15,107 +44,32 @@ const Course = () => {
             {getText("ourCourses")}
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 justify-items-center">
-            <div
-              className="relative h-48 w-80 bg-cover bg-center rounded-3xl overflow-hidden shadow-lg transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
-              style={{
-                backgroundImage: "url('/math.jpg')",
-              }}
-            >
-              {/* Overlay */}
-              <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-3xl transition-opacity duration-500 hover:opacity-80"></div>
+            {[
+              { image: "/math.jpg", text: getText("math") },
+              { image: "/english.jpg", text: getText("english") },
+              { image: "/russian.jpg", text: getText("russian") },
+              { image: "/ielts.webp", text: getText("ielts") },
+              { image: "/sat.jpg", text: getText("sat") },
+              { image: "/fizika.jpg", text: getText("physics") },
+            ].map((course, index) => (
+              <div
+                key={index}
+                className="course-card relative h-48 w-80 bg-cover bg-center rounded-3xl overflow-hidden shadow-lg transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
+                style={{
+                  backgroundImage: `url(${course.image})`,
+                }}
+              >
+                {/* Overlay */}
+                <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-3xl transition-opacity duration-500 hover:opacity-80"></div>
 
-              {/* Content */}
-              <div className="absolute bottom-5 left-6 text-white">
-                <h4 className="font-bold text-lg transition-transform duration-300 ease-in-out transform hover:translate-y-1">
-                  {getText("math")}
-                </h4>
+                {/* Content */}
+                <div className="absolute bottom-5 left-6 text-white">
+                  <h4 className="font-bold text-lg transition-transform duration-300 ease-in-out transform hover:translate-y-1">
+                    {course.text}
+                  </h4>
+                </div>
               </div>
-            </div>
-
-            <div
-              className="relative h-48 w-80 bg-cover bg-center rounded-3xl overflow-hidden shadow-lg transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
-              style={{
-                backgroundImage: "url('/english.jpg')",
-              }}
-            >
-              {/* Overlay */}
-              <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-3xl transition-opacity duration-500 hover:opacity-80"></div>
-
-              {/* Content */}
-              <div className="absolute bottom-5 left-6 text-white">
-                <h4 className="font-bold text-lg transition-transform duration-300 ease-in-out transform hover:translate-y-1">
-                  {getText("english")}
-                </h4>
-              </div>
-            </div>
-
-            <div
-              className="relative h-48 w-80 bg-cover bg-center rounded-3xl overflow-hidden shadow-lg transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
-              style={{
-                backgroundImage: "url('/russian.jpg')",
-              }}
-            >
-              {/* Overlay */}
-              <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-3xl transition-opacity duration-500 hover:opacity-80"></div>
-
-              {/* Content */}
-              <div className="absolute bottom-5 left-6 text-white">
-                <h4 className="font-bold text-lg transition-transform duration-300 ease-in-out transform hover:translate-y-1">
-                  {getText("russian")}
-                </h4>
-              </div>
-            </div>
-
-            <div
-              className="relative h-48 w-80 bg-cover bg-center rounded-3xl overflow-hidden shadow-lg transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
-              style={{
-                backgroundImage: "url('/ielts.webp')",
-              }}
-            >
-              {/* Overlay */}
-              <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-3xl transition-opacity duration-500 hover:opacity-80"></div>
-
-              {/* Content */}
-              <div className="absolute bottom-5 left-6 text-white">
-                <h4 className="font-bold text-lg transition-transform duration-300 ease-in-out transform hover:translate-y-1">
-                  {getText("ielts")}
-                </h4>
-              </div>
-            </div>
-
-            <div
-              className="relative h-48 w-80 bg-cover bg-center rounded-3xl overflow-hidden shadow-lg transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
-              style={{
-                backgroundImage: "url('/sat.jpg')",
-              }}
-            >
-              {/* Overlay */}
-              <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-3xl transition-opacity duration-500 hover:opacity-80"></div>
-
-              {/* Content */}
-              <div className="absolute bottom-5 left-6 text-white">
-                <h4 className="font-bold text-lg transition-transform duration-300 ease-in-out transform hover:translate-y-1">
-                  {getText("sat")}
-                </h4>
-              </div>
-            </div>
-
-            <div
-              className="relative h-48 w-80 bg-cover bg-center rounded-3xl overflow-hidden shadow-lg transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
-              style={{
-                backgroundImage: "url('/fizika.jpg')",
-              }}
-            >
-              {/* Overlay */}
-              <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-3xl transition-opacity duration-500 hover:opacity-80"></div>
-
-              {/* Content */}
-              <div className="absolute bottom-5 left-6 text-white">
-                <h4 className="font-bold text-lg transition-transform duration-300 ease-in-out transform hover:translate-y-1">
-                  {getText("physics")}
-                </h4>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
