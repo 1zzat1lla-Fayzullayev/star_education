0afc434fbe2a56865c04e306e25229ded2bcd00b
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2024-11-06 20:06:21 +0500

    change img path

--- src/components/course.jsx
+++ src/components/course.jsx
@@ -11,13 +11,14 @@ const Course = () => {
     <>
       <div className="flex justify-center items-center py-20 bg-gray-100">
         <div className="container mx-auto">
-         <h2 className="text-[25px] md:text-[30px] lg:text-[50px] font-[600] text-center mb-[30px] text-gray-800">{getText("ourCourses")}</h2>
+          <h2 className="text-[25px] md:text-[30px] lg:text-[50px] font-[600] text-center mb-[30px] text-gray-800">
+            {getText("ourCourses")}
+          </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 justify-items-center">
             <div
               className="relative h-48 w-80 bg-cover bg-center rounded-3xl overflow-hidden shadow-lg transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
               style={{
-                backgroundImage:
-                  "url('https://www.youcubed.org/wp-content/uploads/2017/03/beautiful-math-1.jpg')",
+                backgroundImage: "url('/math.jpg')",
               }}
             >
               {/* Overlay */}
@@ -34,8 +35,7 @@ const Course = () => {
             <div
               className="relative h-48 w-80 bg-cover bg-center rounded-3xl overflow-hidden shadow-lg transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
               style={{
-                backgroundImage:
-                  "url('https://t3.ftcdn.net/jpg/03/70/42/66/360_F_370426690_Pejt9KxjWTHPklsKwripaxr0iA17zupF.jpg')",
+                backgroundImage: "url('/english.jpg')",
               }}
             >
               {/* Overlay */}
@@ -52,8 +52,7 @@ const Course = () => {
             <div
               className="relative h-48 w-80 bg-cover bg-center rounded-3xl overflow-hidden shadow-lg transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
               style={{
-                backgroundImage:
-                  "url('https://media.istockphoto.com/id/506675034/photo/question-do-you-speak-russian-written-in-russian.jpg?s=612x612&w=0&k=20&c=XeFuy9wa_CnUBgMuCxm4dw2S4cLAf9NES8azAH5mjAU=')",
+                backgroundImage: "url('/russian.jpg')",
               }}
             >
               {/* Overlay */}
@@ -70,8 +69,7 @@ const Course = () => {
             <div
               className="relative h-48 w-80 bg-cover bg-center rounded-3xl overflow-hidden shadow-lg transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
               style={{
-                backgroundImage:
-                  "url('https://esdubai.com/wp-content/uploads/2023/01/Why-Do-I-Need-IELTS-1.jpg')",
+                backgroundImage: "url('/ielts.webp')",
               }}
             >
               {/* Overlay */}
@@ -88,8 +86,7 @@ const Course = () => {
             <div
               className="relative h-48 w-80 bg-cover bg-center rounded-3xl overflow-hidden shadow-lg transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
               style={{
-                backgroundImage:
-                  "url('https://edwisefoundation.com/wp-content/uploads/2021/02/What-SAT-1-Is.jpg')",
+                backgroundImage: "url('/sat.jpg')",
               }}
             >
               {/* Overlay */}
@@ -106,8 +103,7 @@ const Course = () => {
             <div
               className="relative h-48 w-80 bg-cover bg-center rounded-3xl overflow-hidden shadow-lg transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
               style={{
-                backgroundImage:
-                  "url('https://t4.ftcdn.net/jpg/02/14/56/75/360_F_214567514_hGbTMUq06pJIGKiXA248l43E3hU9Q08x.jpg')",
+                backgroundImage: "url('/fizika.jpg')",
               }}
             >
               {/* Overlay */}
