b435785d02320475572038324e9a6c642065027d
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2025-04-23 18:14:12 +0500

    add number of student

---
+++ src/components/numberOfStudents.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Wrapper from "../layout/wrapper";
+import CountUp from "react-countup";
+
+function NumberOfStudents() {
+  const stats = [
+    { label: "O'quvchilar soni", value: 900 },
+    { label: "O'qishga kirganlar soni", value: 899 },
+    { label: "Tajriba (yil)", value: 20 },
+  ];
+
+  return (
+    <Wrapper>
+      <div className="flex flex-wrap justify-between gap-6 p-8">
+        {stats.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-start text-[20px] min-w-[180px]"
+          >
+            <span className="text-gray-600">{item.label}</span>
+            <span className="text-3xl font-bold text-black mt-2">
+              <CountUp
+                end={item.value}
+                duration={5}
+                suffix={item.label.includes("Tajriba") ? "+" : "+"}
+              />
+            </span>
+          </div>
+        ))}
+      </div>
+    </Wrapper>
+  );
+}
+
+export default NumberOfStudents;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/header";
+import NumberOfStudents from "../components/numberOfStudents";
 
 function Home() {
   return (
     <>
       <Header />
+      <NumberOfStudents />
     </>
   );
 }
